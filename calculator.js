3d5b354f40661c99ce9161326b7a3849bd558265
Willv97 <willv1357@gmail.com>
2024-06-04 13:48:02 -0400

    Changed display color to white, created a function for append to display, clear display and calculate the results. Fixed a html where I was missing a letter

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// Calculator
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+
+}
+
+function calculate() {
+
+}
