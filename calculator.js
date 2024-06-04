abe994b42e746e69f6e0233ec928bca15e74f4c9
Willv97 <willv1357@gmail.com>
2024-06-04 13:53:15 -0400

    Added a try block to catch any errors.

--- calculator.js
+++ calculator.js
@@ -7,9 +7,15 @@ function appendToDisplay(input) {
 }
 
 function clearDisplay() {
-
+    display.value = "";
 }
 
 function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
 
+    catch {
+        display.value = "Error";
+    }
 }
