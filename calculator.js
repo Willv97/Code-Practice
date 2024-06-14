0c5b1260a066b50753055acc8511ef67f663c3dc
Willv97 <willv1357@gmail.com>
2024-06-13 23:47:19 -0400

    Added comments in calculator.js

--- calculator.js
+++ calculator.js
@@ -2,10 +2,12 @@
 
 const display = document.getElementById("display");
 
+//Create a function to add values to the display variable
 function appendToDisplay(input) {
     display.value += input;
 }
 
+//Create a function to clear the value of display
 function clearDisplay() {
     display.value = "";
 }
