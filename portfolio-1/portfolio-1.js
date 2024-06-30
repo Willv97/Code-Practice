c9e72c6c09ae0cb465599815c67c88e74bcfdca0
Willv97 <willv1357@gmail.com>
2024-06-29 23:46:08 -0400

    New comments in js file

--- portfolio-1/portfolio-1.js
+++ portfolio-1/portfolio-1.js
@@ -1,3 +1,4 @@
+// Function to open and close menu
 function toggleMenu() {
     const menu = document.querySelector(".menu-links"); //Target elements
     const icon = document.querySelector(".hamburger-icon");
